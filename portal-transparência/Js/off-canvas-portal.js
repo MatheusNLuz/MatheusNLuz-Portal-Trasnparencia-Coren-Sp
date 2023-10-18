9f37baa070963d4e68afc05f6c3c773f04a98584
Matheus Luz <105671660+MatheusNLuz@users.noreply.github.com>
2023-10-18 19:31:50 +0000

    criação de mais um offcanvas para mobile

---
+++ portal-transparência/Js/off-canvas-portal.js
@@ -0,0 +1,13 @@
+const openBtnDoPortal = document.querySelector('.menu-toggle-do-portal');
+const closeBtnDoPortal = document.querySelector('.btn-close-portal');
+const offCanvasDoPortal = document.querySelector('.offcanvas-menu-portal');
+
+openBtnDoPortal.addEventListener('click', (e) => {
+    e.preventDefault();
+    offCanvasDoPortal.classList.add('active');
+})
+
+closeBtnDoPortal.addEventListener('click', (e) => {
+    e.preventDefault();
+    offCanvasDoPortal.classList.remove('active')
+})
